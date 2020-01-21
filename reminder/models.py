8c73711b077e09059c5d136b115fa309c4b40685
wilbrone <wilbroneokoth@gmail.com>
2020-01-21 18:04:19 +0300

    add profile model

--- reminder/models.py
+++ reminder/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class Profile(models.Model):
+    name=models.CharField(max_length=60)
+    bio=models.CharField(max_length=300)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image=models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
+    def create_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
